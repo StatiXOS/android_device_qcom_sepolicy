36e73760083819d214a5108f502fb1066a8842a0
Anay Wadhera <anay1018@gmail.com>
2019-09-15 22:01:44 -0700

    sepolicy: move to relative paths
    - this was done to avoid issues on some systems where LOCAL_PATH
    would not be picked up correctly and would result in a ninja deps
    issue
    
    Change-Id: I8fd2ab423c5c9dca319b222bfe84728091563f7f

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,54 +1,53 @@
 # Board specific SELinux policy variable definitions
-LOCAL_PATH:= $(call my-dir)
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    $(LOCAL_PATH)/generic/public
+    device/qcom/sepolicy/generic/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    $(LOCAL_PATH)/generic/private
+    device/qcom/sepolicy/generic/private
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    $(LOCAL_PATH)/qva/public
+    device/qcom/sepolicy/qva/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    $(LOCAL_PATH)/qva/private
+    device/qcom/sepolicy/qva/private
 
 ifeq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
     BOARD_SEPOLICY_DIRS += \
-       $(LOCAL_PATH) \
-       $(LOCAL_PATH)/generic/vendor/common \
-       $(LOCAL_PATH)/qva/vendor/common/sysmonapp \
-       $(LOCAL_PATH)/qva/vendor/ssg \
-       $(LOCAL_PATH)/generic/vendor/timeservice \
-       $(LOCAL_PATH)/qva/vendor/common
+       device/qcom/sepolicy \
+       device/qcom/sepolicy/generic/vendor/common \
+       device/qcom/sepolicy/qva/vendor/common/sysmonapp \
+       device/qcom/sepolicy/qva/vendor/ssg \
+       device/qcom/sepolicy/generic/vendor/timeservice \
+       device/qcom/sepolicy/qva/vendor/common
 
     ifeq ($(TARGET_SEPOLICY_DIR),)
-      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/generic/vendor/$(TARGET_BOARD_PLATFORM)
-      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/qva/vendor/$(TARGET_BOARD_PLATFORM)
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/generic/vendor/$(TARGET_BOARD_PLATFORM)
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/qva/vendor/$(TARGET_BOARD_PLATFORM)
     else
-      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/generic/vendor/$(TARGET_SEPOLICY_DIR)
-      BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/qva/vendor/$(TARGET_SEPOLICY_DIR)
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/generic/vendor/$(TARGET_SEPOLICY_DIR)
+      BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/qva/vendor/$(TARGET_SEPOLICY_DIR)
     endif
 
     ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-        BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/generic/vendor/test
-        BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/qva/vendor/test
+        BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/generic/vendor/test
+        BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/qva/vendor/test
     endif
 endif
 
 ifneq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
     BOARD_SEPOLICY_DIRS += \
-         $(LOCAL_PATH) \
-         $(LOCAL_PATH)/legacy/vendor/common/sysmonapp \
-         $(LOCAL_PATH)/legacy/vendor/ssg \
-         $(LOCAL_PATH)/legacy/vendor/timeservice \
-         $(LOCAL_PATH)/legacy/vendor/common
+         device/qcom/sepolicy \
+         device/qcom/sepolicy/legacy/vendor/common/sysmonapp \
+         device/qcom/sepolicy/legacy/vendor/ssg \
+         device/qcom/sepolicy/legacy/vendor/timeservice \
+         device/qcom/sepolicy/legacy/vendor/common
 
     ifeq ($(TARGET_SEPOLICY_DIR),)
-        BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/legacy/vendor/$(TARGET_BOARD_PLATFORM)
+        BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy/vendor/$(TARGET_BOARD_PLATFORM)
     else
-        BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/legacy/vendor/$(TARGET_SEPOLICY_DIR)
+        BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy/vendor/$(TARGET_SEPOLICY_DIR)
     endif
     ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-        BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/legacy/vendor/test
+        BOARD_SEPOLICY_DIRS += device/qcom/sepolicy/legacy/vendor/test
     endif
 endif
