57e19252ff944d20bf1549cdbe098e594579949f
Anay Wadhera <awadhera@berkeley.edu>
2020-10-09 01:31:36 -0700

    sepolicy: cleanup makefile and include statix qcom sepolicy

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -3,39 +3,32 @@ ifeq ($(call is-vendor-board-platform,QCOM),true)
 SEPOLICY_PATH:= device/qcom/sepolicy_vndr
 QSSI_SEPOLICY_PATH:= device/qcom/sepolicy
 SYS_ATTR_PROJECT_PATH := $(TOP)/device/qcom/sepolicy/generic/public/attribute
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
-    $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
     $(QSSI_SEPOLICY_PATH)/generic/public \
     $(QSSI_SEPOLICY_PATH)/generic/public/attribute
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
-    $(BOARD_PLAT_PRIVATE_SEPOLICY_DIR) \
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     $(QSSI_SEPOLICY_PATH)/generic/private
 
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
-    $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
     $(QSSI_SEPOLICY_PATH)/qva/public \
     $(QSSI_SEPOLICY_PATH)/qva/public/attribute
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
-    $(BOARD_PLAT_PRIVATE_SEPOLICY_DIR) \
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     $(QSSI_SEPOLICY_PATH)/qva/private
 
 #once all the services are moved to Product /ODM above lines will be removed.
 # sepolicy rules for product images
-PRODUCT_PUBLIC_SEPOLICY_DIRS := \
-    $(PRODUCT_PUBLIC_SEPOLICY_DIRS) \
+PRODUCT_PUBLIC_SEPOLICY_DIRS += \
     $(QSSI_SEPOLICY_PATH)/generic/product/public \
     $(QSSI_SEPOLICY_PATH)/qva/product/public
 
-PRODUCT_PRIVATE_SEPOLICY_DIRS := \
-    $(PRODUCT_PRIVATE_SEPOLICY_DIRS) \
+PRODUCT_PRIVATE_SEPOLICY_DIRS += \
     $(QSSI_SEPOLICY_PATH)/generic/product/private \
     $(QSSI_SEPOLICY_PATH)/qva/product/private
 
 ifeq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
-    BOARD_SEPOLICY_DIRS := \
-       $(BOARD_SEPOLICY_DIRS) \
+    BOARD_SEPOLICY_DIRS += \
        $(SEPOLICY_PATH) \
        $(SEPOLICY_PATH)/generic/vendor/common \
        $(SEPOLICY_PATH)/qva/vendor/ssg \
@@ -57,8 +50,7 @@ ifeq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
 endif
 
 ifneq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
-    BOARD_SEPOLICY_DIRS := \
-                 $(BOARD_SEPOLICY_DIRS) \
+    BOARD_SEPOLICY_DIRS += \
                  $(SEPOLICY_PATH) \
                  $(SEPOLICY_PATH)/legacy/vendor/ssg \
                  $(SEPOLICY_PATH)/legacy/vendor/common
@@ -74,3 +66,5 @@ ifneq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
     endif
 endif
 endif
+
+-include device/statix/sepolicy/qcom/sepolicy.mk
